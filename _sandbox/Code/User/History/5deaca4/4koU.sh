cfa5b2021852b533069e0e9918eddb4d1c576d1f
aonthefriend <aon.thefriend@gmail.com>
2025-05-27 20:44:21 -0300

    folder _sandbox criado para guardar os arquivos do Code

--- _sandbox/Code/User/History/5deaca4/4koU.sh
+++ _sandbox/Code/User/History/5deaca4/4koU.sh
@@ -1,15 +1,17 @@
-# Cria diretórios necessários
+#!/usr/bin/env bash
+
+# Cria diretórios necessários no ~/.config
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/kitty
 mkdir -p ~/.config/zsh
-mkdir -p "$HOME/dotfiles/_sandbox/code"
+mkdir -p ~/.hidden/code
 
 # Cria symlinks dos arquivos versionados
 ln -sf "$HOME/dotfiles/hypr/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
 ln -sf "$HOME/dotfiles/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh/.zshrc"
-ln -sf "$HOME/dotfiles/zsh/.p10k.zsh" "$HOME/.config/zsh/.p10k.zsh"
-ln -sf "$HOME/dotfiles/_sandbox/code" "$HOME/code"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/dotfiles/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -s ~/.hidden/code ~/code
 
 # Mensagem de sucesso
 echo "Symlinks criados com sucesso."
