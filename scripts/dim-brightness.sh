3a4009c1c443e28a6cdfac8bc8a5154ad3d6a1fd
aonthefriend <aon.thefriend@gmail.com>
2025-06-26 18:06:07 -0300

    atualização nos arquivos brightness para um brilho automátimo após a tela ser bloqueada retornar no brilho anterior. E hyprland.conf modificado para melhor adaptação dessas modificações

--- scripts/dim-brightness.sh
+++ scripts/dim-brightness.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Armazena brilho atual em um arquivo temporário
+brightnessctl get > /tmp/last-brightness
+
+# Diminui o brilho para 10%
 brightnessctl set 10%
--- scripts/restore-brightness.sh
+++ scripts/restore-brightness.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-brightnessctl set 100%
+# Verifica se o arquivo existe
+if [[ -f /tmp/last-brightness ]]; then
+    saved=$(cat /tmp/last-brightness)
+    brightnessctl set "$saved"
+else
+    brightnessctl set 100%
+fi
